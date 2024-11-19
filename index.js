dc098292a11d024c34aa82fa95112563fa8fe74e
leticia <leticiavillela62@gmail.com>
2024-11-19 17:02:58 -0500

    new lines

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 
 async function main() {
-    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=21391f58");
+    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=21391f58&s=movies");
     const movieData = await movies.json();
     const movieList = document.querySelector(".movie-list");
-    movieList.innerHTML = movieData.map((movies) => moviesHTML(movie)).join("");
+    movieList.innerHTML = movieData.map((movies) => moviesHTML(movies)).join("");
 }
 
 main();
 
-function movieList(movie) {
+function showMovieList(movie) {
     localStorage.setItem("movie",movie);
     window.location.href = `${window.location.origin}/movie.html`;
-}
-
-
-
-function moviesHTML(movie) {
-    return `<div class="movie__wrapper" onclick="showMovieList(${movie})>
-                        <div class="movie__data">
-                            <img src="" alt="">
-                            <h3>${movie.name}</h3>
-                            <p class="price">${movie.price}</p>
-                        </div>
-                    </div>`
 }
 
 
--- movies.js
+++ movies.js
@@ -1,41 +1,96 @@
+function renderMovies() {
+  const movieWrapper = document.querySelector('.movie__wrapper');
+
+  const movies = getMovies();
+  console.log(movies)
+  movieWrapper.innerHTML = 
+  `<div class="movie__wrapper">
+            <div class="movie__data">
+              <figure>
+              <img src="${poster.url}" class="movie__poster" alt=""></figure>
+              <h3 class="movie__name">${movie.title}</h3>
+              <p class="movie__year">${movie.year}</p>
+            </div>
+          </div>`
+}
+setTimeout(() => {
+  renderMovies();
+});
+
 /* DATA */ 
-/*movieData {
-    "Title": "Guardians of the Galaxy Vol. 2",
-    "Year": "2017",
-    "Rated": "PG-13",
-    "Released": "05 May 2017",
-    "Runtime": "136 min",
-    "Genre": "Action, Adventure, Comedy",
-    "Director": "James Gunn",
-    "Writer": "James Gunn, Dan Abnett, Andy Lanning",
-    "Actors": "Chris Pratt, Zoe Saldana, Dave Bautista",
-    "Plot": "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father, the ambitious celestial being Ego.",
-    "Language": "English",
-    "Country": "United States",
-    "Awards": "Nominated for 1 Oscar. 15 wins & 60 nominations total",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg",
-    "Ratings": [
-      {
-        "Source": "Internet Movie Database",
-        "Value": "7.6/10"
-      },
-      {
-        "Source": "Rotten Tomatoes",
-        "Value": "85%"
-      },
-      {
-        "Source": "Metacritic",
-        "Value": "67/100"
-      }
-    ],
-    "Metascore": "67",
-    "imdbRating": "7.6",
-    "imdbVotes": "774,852",
-    "imdbID": "tt3896198",
-    "Type": "movie",
-    "DVD": "N/A",
-    "BoxOffice": "$389,813,101",
-    "Production": "N/A",
-    "Website": "N/A",
-    "Response": "True"
-  }*/
+function getMovies() {
+  return [
+    {
+      "Title": "Teen Titans GO! To the Movies",
+      "Year": "2018",
+      "imdbID": "tt7424200",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNTU5YjQxNzAtNzkxNC00MGVjLWFhMjgtMjg2ZDUyM2Q5YzdhXkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+      "Title": "The Life and Movies of Ersan Kuneri",
+      "Year": "2022–",
+      "imdbID": "tt13238304",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNjRkMjQ2Y2MtMTQ5NC00NjljLTg0M2EtNTllZGRhYjc1ZTA2XkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Home Movies",
+      "Year": "1999–2004",
+      "imdbID": "tt0197159",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMzIwZDVkZmYtNjJhOC00YTJjLWFlYzMtMDhhNjdhNDEwYTE2XkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+      "Title": "The Movies That Made Us",
+      "Year": "2019–2021",
+      "imdbID": "tt10681222",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzVhNTdmNGMtNzQ0My00MmE1LTllNTEtOGEwNmFiNzY0NDFlXkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+      "Title": "The New Scooby-Doo Movies",
+      "Year": "1972–1973",
+      "imdbID": "tt0068112",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMmQwMzY4MDYtMGY1Ny00YTQzLWExZjQtNGJhNWJiMTFkYTM0XkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+      "Title": "The Unforgettable Director of Love Movies",
+      "Year": "1990",
+      "imdbID": "tt0263074",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BZWZlODEzYjgtZTJlNS00YmEyLTlkZDYtOWUwYjQ5ZmZhYzliXkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+      "Title": "A Personal Journey with Martin Scorsese Through American Movies",
+      "Year": "1995",
+      "imdbID": "tt0112120",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BZGRkMWFkZjgtYzc2My00MjljLTkwNTEtMGZhOTUyM2I3OGMxL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+    },
+    {
+      "Title": "I Like Movies",
+      "Year": "2022",
+      "imdbID": "tt14913282",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYjZhMmI5OTgtODVjYi00N2NlLWE4YWQtMmQwNzdkZGU2Mzc3XkEyXkFqcGc@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Midnight Movies: From the Margin to the Mainstream",
+      "Year": "2005",
+      "imdbID": "tt0457414",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTIzNTc4MzQyMF5BMl5BanBnXkFtZTcwODM1OTc3MQ@@._V1_SX300.jpg"
+    },
+    {
+      "Title": "The Movies",
+      "Year": "2019",
+      "imdbID": "tt10468676",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMGMzZjdiNjEtMTMwMS00MTVkLTk0NWMtOGFjODY1MWRkNmVmXkEyXkFqcGc@._V1_SX300.jpg"
+    }
+  ],
+  "totalResults"; "999",
+  "Response"; "True"
+}
